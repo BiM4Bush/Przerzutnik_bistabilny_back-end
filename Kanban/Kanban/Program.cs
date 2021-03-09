1d4a72db643fc63e32fe438cb16ed06cdf72e431
BiM4Bush <sebakrzynek@gmial.com>
2021-03-09 17:15:45 +0100

    First code.

---
+++ Kanban/Kanban/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appName = "Kanban table";
+            int appVersion = 1;
+            string [] authors = new string [6];
+            authors[0] = "Sebastian Krzynówek";
+            authors[1] = "Mateusz Kowalewski";
+            authors[2] = "Paweł Kulesza";
+            authors[3] = "Marcin Kozłowski";
+            authors[4] = "Marcin Krzemiński";
+            authors[5] = "Michał Wołyniec";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Aplikacja {appName}. Wersja {appVersion}.");
+            Console.Write("Authors: ");
+            foreach (string author in authors)
+            {
+                Console.Write(author+ "  "); 
+            }
+            Console.ResetColor();
+            Column ToDo = new Column("To do");
+            Column InProgress = new Column("In progress");
+            Column Testing = new Column("Testing");
+            Column Done = new Column("Done");
+
+            Note A = new Note("A", "Pierwsza karteczka", Testing);
+            Note B = new Note("B", "Druga karteczka", Testing);
+            Note C = new Note("C", " ", Testing);
+            Note D = new Note("D", " ", InProgress);
+            Note F = new Note("F", " ", ToDo);
+            Note H = new Note("H", " ", InProgress);
+            Note I = new Note("I", " ", InProgress);
+            Note K = new Note("K", " ", Done);
+            Note S = new Note("S", " ", ToDo);
+
+            var Kanban = new Dictionary<string, string>()
+            {
+                {F.name, ToDo.name},
+                {S.name, ToDo.name},
+                {D.name, InProgress.name},
+                {H.name, InProgress.name},
+                {I.name, InProgress.name},
+                {A.name, Testing.name},
+                {B.name, Testing.name},
+                {C.name, Testing.name},
+                {K.name, Done.name}
+            };
+
+            foreach (KeyValuePair<string, string> kvp in Kanban)
+            {
+                Console.WriteLine("Karteczka {0} jest w kolumnie {1}", kvp.Key, kvp.Value);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Column
+    {
+        public string name;
+
+        public Column(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    public class Note
+    {
+        public string name;
+        private string description;
+        private Column column;
+
+        public Note(string name, string description, Column column)
+        {
+            this.name = name;
+            this.description = description;
+            this.column = column;
+        }
+
+    }
+}
---
+++ Kanban/Kanban/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Kanban")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kanban")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("6c1791c8-e29d-4894-ae61-7f27ab2e9386")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
